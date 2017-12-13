61219dc60c56e77b4c5154431e85d6459cb2d53f
Stefan Illgen <stefan.illgen@ottogroup.com>
2017-12-13 13:50:22 +0100

    Elasticsearch Java REST Client: search asynchronously

--- search/java-client/src/test/java/SearchApiTest.java
+++ search/java-client/src/test/java/SearchApiTest.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
@@ -17,16 +18,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 public class SearchApiTest {
 
   private RestHighLevelClient client;
+  private SearchRequest searchRequest;
 
   @Before
   public void setUp() throws Exception {
     client = new RestHighLevelClient(
         RestClient.builder(
             new HttpHost("localhost", 9200, "http")));
+    final SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+    sourceBuilder.query(QueryBuilders.wildcardQuery("name", "t?s*"));
+    sourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
+    sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+    sourceBuilder.size(5);
+    searchRequest = new SearchRequest(new String[]{"arteria"}, sourceBuilder);
+    searchRequest.types("product");
   }
 
   @After
@@ -36,14 +46,6 @@ public class SearchApiTest {
 
   @Test
   public void shouldFindSynchronously() throws Exception {
-    final SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-    sourceBuilder.query(QueryBuilders.wildcardQuery("name", "t?s*"));
-    sourceBuilder.sort(new ScoreSortBuilder().order(SortOrder.DESC));
-    sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
-    sourceBuilder.size(5);
-    final SearchRequest searchRequest = new SearchRequest(new String[]{"arteria"}, sourceBuilder);
-    searchRequest.types("product");
-
     final SearchResponse searchResponse = client.search(searchRequest);
 
     assertThat(searchResponse).isNotNull();
@@ -57,5 +59,59 @@ public class SearchApiTest {
     assertThat(firstHit.getScore()).isEqualTo(1.0f);
     assertThat(firstHit.getSourceAsString())
         .isEqualTo("{\"name\":\"Tisch1\",\"description\":\"Ein Tisch 1\"}");
+  }
+
+  @Test
+  public void shouldFindAsynchronously() throws Exception {
+    client.searchAsync(searchRequest, new ActionListener<SearchResponse>() {
+      @Override
+      public void onResponse(final SearchResponse searchResponse) {
+        assertThat(searchResponse).isNotNull();
+      }
+
+      @Override
+      public void onFailure(final Exception ex) {
+        fail("Search Failure", ex);
+      }
+    });
+  }
+
+  @Test
+  public void shouldFindMatchQuery() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shouldFindWidlcardQuery() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shouldFindTermQuery() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shouldSortDesc() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shouldHighlightFragments() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shoulkdRetrieveAggregations() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void shouldRetrieveSuggestions() throws Exception {
+    // todo
+  }
+
+  @Test
+  public void name() throws Exception {
   }
 }
